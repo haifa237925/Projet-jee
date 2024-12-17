8a757d2aa55abc35710fb93436a7d52b0d7be86e
haifa237925 <haifa.helaoui@etudiant-fst.utm.tn>
2024-12-17 20:40:37 +0100

    Premier commit de mon projet Spring Boot

---
+++ src/test/java/com/gestion/covoiturage/GestionCovoiturageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.covoiturage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCovoiturageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
